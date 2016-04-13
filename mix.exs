941373731b74857a16343f52965dcc029b8f9ae3
zhongwencool <zhongwencool@gmail.com>
2016-04-13 14:25:02 +0800

    publish on hex 1.0.0

--- mix.exs
+++ mix.exs
@@ -3,27 +3,37 @@ defmodule Maxwell.Mixfile do
 
   def project do
     [app: :maxwell,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: [
+       maintainers: ["zhongwencool"],
+       links: %{"GitHub" => "https://github.com/zhongwencool/maxwell"},
+       files: ~w(lib LICENSE mix.exs README.md),
+       description: """
+       Maxwell is an HTTP client that provides a common interface over many adapters (such as hackney, ibrowse) and embraces the concept of Rack middleware when processing the request/response cycle.
+       """,
+       licenses: ["MIT"]
+     ],
      test_coverage: [tool: ExCoveralls],
      deps: deps]
   end
 
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :poison,]]
+    [applications: [:logger]]
   end
 
   defp deps do
     [
-     {:mimerl, "~> 1.0.2"}, # for find multipart ctype
-     {:ibrowse, "~> 4.2", optional: true, only: :test},
-     {:poison, "~> 2.1", optional: true, only: :test},
-     {:hackney, "~> 1.6", optional: true, only: :test},
-     {:excoveralls, "~> 0.5.1", only: :test},
+      {:mimerl, "~> 1.0.2"}, # for find multipart ctype
+      {:poison, "~> 2.1", only: :test, optional: true},
+      {:ibrowse, "~> 4.2", only: :test, optional: true},
+      {:hackney, "~> 1.6", only: :test, optional: true},
+      {:excoveralls, "~> 0.5.1", only: :test},
     ]
   end
 
 end
+
