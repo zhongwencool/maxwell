54283828677237dbca6866045bc55e131d843b43
zhongwencool <zhongwencool@gmail.com>
2016-07-29 20:30:03 +0800

    bump to 1.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Maxwell.Mixfile do
 
   def project do
     [app: :maxwell,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
