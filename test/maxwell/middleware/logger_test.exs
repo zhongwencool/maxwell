d7cc31700d26a8e9c05e99b363046e93bf7849f0
Xiangrong Hao <falood@gmail.com>
2017-02-13 07:29:53 -0800

    update log level setting, close #44 (#45)
    * update log level setting, close #44
    
    * fix coveralls
    
    * fix issues by secretworry's review
    
    * update docs

--- test/maxwell/middleware/logger_test.exs
+++ test/maxwell/middleware/logger_test.exs
@@ -17,21 +17,24 @@ defmodule LoggerTest do
   test "Logger Call" do
     conn = %Conn{method: :get, url: "http://example.com", status: 200}
     outputstr = capture_log fn ->
-      Maxwell.Middleware.Logger.call(conn,fn(x) ->{:error, "bad request", %{x| status: 400}} end, :info) end
-    output301 = capture_log fn -> Maxwell.Middleware.Logger.call(conn, fn(x) -> %{x| status: 301} end, :info) end
-    output404 = capture_log fn -> Maxwell.Middleware.Logger.call(conn, fn(x) -> %{x| status: 404} end, :info) end
-    output500 = capture_log fn -> Maxwell.Middleware.Logger.call(conn, fn(x) -> %{x| status: 500} end, :info) end
-    outputok  = capture_log fn -> Maxwell.Middleware.Logger.call(conn, fn(x) -> x end, :info) end
-    assert outputstr =~ ~r"\e\[22m\n\d+:\d+:\d+.\d+ \[info\]  GET http://example.com>> \e\[31mERROR: <<\"bad request\">>\n\e\[0m"
+      Maxwell.Middleware.Logger.call(conn,fn(x) ->{:error, "bad request", %{x| status: 400}} end, [default: :info])
+    end
+    output301 = capture_log fn -> Maxwell.Middleware.Logger.call(conn, fn(x) -> %{x| status: 301} end, [default: :error]) end
+    output404 = capture_log fn -> Maxwell.Middleware.Logger.call(conn, fn(x) -> %{x| status: 404} end, [{404, :debug}]) end
+    output500 = capture_log fn -> Maxwell.Middleware.Logger.call(conn, fn(x) -> %{x| status: 500} end, [{500..599, :warn}, {:default, :error}]) end
+    outputok  = capture_log fn -> Maxwell.Middleware.Logger.call(conn, fn(x) -> x end, [default: :info]) end
+    nooutput  = capture_log fn -> Maxwell.Middleware.Logger.call(conn, fn(x) -> x end, [{400.599, :info}]) end
+    assert outputstr =~ ~r"\e\[31m\n\d+:\d+:\d+.\d+ \[error\] GET http://example.com>> \e\[31mERROR: \"bad request\"\n\e\[0m"
 
-    assert output301 =~ ~r"\e\[22m\n\d+:\d+:\d+.\d+ \[info\]  get http://example.com <<<\e\[33m301\(\d+.\d+ms\)\e\[0m\n%Maxwell.Conn\{method: :get, opts: \[\], path: \"\", query_string: \%\{\}, req_body: nil, req_headers: \%\{\}, resp_body: \"\", resp_headers: \%\{\}, state: :unsent, status: 301, url: \"http://example.com\"\}\n\e\[0m"
+    assert output301 =~ ~r"\e\[31m\n\d+:\d+:\d+.\d+ \[error\] get http://example.com <<<\e\[31m301\(\d+.\d+ms\)\e\[0m\n%Maxwell.Conn\{method: :get, opts: \[\], path: \"\", query_string: \%\{\}, req_body: nil, req_headers: \%\{\}, resp_body: \"\", resp_headers: \%\{\}, state: :unsent, status: 301, url: \"http://example.com\"\}\n\e\[0m"
 
-    assert output404 =~ ~r"\e\[22m\n\d+:\d+:\d+.\d+ \[info\]  get http://example.com <<<\e\[31m404\(\d+.\d+ms\)\e\[0m\n%Maxwell.Conn\{method: :get, opts: \[\], path: \"\", query_string: \%\{\}, req_body: nil, req_headers: \%\{\}, resp_body: \"\", resp_headers: \%\{\}, state: :unsent, status: 404, url: \"http://example.com\"\}\n\e\[0m"
+    assert output404 =~ ~r"\e\[36m\n\d+:\d+:\d+.\d+ \[debug\] get http://example.com <<<\e\[36m404\(\d+.\d+ms\)\e\[0m\n%Maxwell.Conn\{method: :get, opts: \[\], path: \"\", query_string: \%\{\}, req_body: nil, req_headers: \%\{\}, resp_body: \"\", resp_headers: \%\{\}, state: :unsent, status: 404, url: \"http://example.com\"\}\n\e\[0m"
 
-    assert output500 =~ ~r"\e\[22m\n\d+:\d+:\d+.\d+ \[info\]  get http://example.com <<<\e\[31m500\(\d+.\d+ms\)\e\[0m\n%Maxwell.Conn\{method: :get, opts: \[\], path: \"\", query_string: \%\{\}, req_body: nil, req_headers: \%\{\}, resp_body: \"\", resp_headers: \%\{\}, state: :unsent, status: 500, url: \"http://example.com\"\}\n\e\[0m"
+    assert output500 =~ ~r"\e\[33m\n\d+:\d+:\d+.\d+ \[warn\]  get http://example.com <<<\e\[33m500\(\d+.\d+ms\)\e\[0m\n%Maxwell.Conn\{method: :get, opts: \[\], path: \"\", query_string: \%\{\}, req_body: nil, req_headers: \%\{\}, resp_body: \"\", resp_headers: \%\{\}, state: :unsent, status: 500, url: \"http://example.com\"\}\n\e\[0m"
 
-    assert outputok  =~ ~r"\e\[22m\n\d+:\d+:\d+.\d+ \[info\]  get http://example.com <<<\e\[32m200\(\d+.\d+ms\)\e\[0m\n%Maxwell.Conn\{method: :get, opts: \[\], path: \"\", query_string: \%\{\}, req_body: nil, req_headers: \%\{\}, resp_body: \"\", resp_headers: \%\{\}, state: :unsent, status: 200, url: \"http://example.com\"}\n\e\[0m"
+    assert outputok  =~ ~r"\e\[22m\n\d+:\d+:\d+.\d+ \[info\]  get http://example.com <<<\e\[22m200\(\d+.\d+ms\)\e\[0m\n%Maxwell.Conn\{method: :get, opts: \[\], path: \"\", query_string: \%\{\}, req_body: nil, req_headers: \%\{\}, resp_body: \"\", resp_headers: \%\{\}, state: :unsent, status: 200, url: \"http://example.com\"}\n\e\[0m"
 
+    assert nooutput == ""
   end
 
   test "Change Middleware Logger's log_level" do
@@ -45,7 +48,7 @@ defmodule LoggerTest do
   end
 
   test "Middleware Logger with invalid log_level" do
-    assert_raise ArgumentError, "Logger Middleware :log_level only accepts atom", fn ->
+    assert_raise ArgumentError, ~r/Logger Middleware: level only accepts/, fn ->
       defmodule TAtom1 do
         use Maxwell.Builder, [:get, :post]
         middleware Maxwell.Middleware.Logger, [log_level: 1234]
@@ -59,6 +62,69 @@ defmodule LoggerTest do
       defmodule TAtom2 do
         use Maxwell.Builder, [:get, :post]
         middleware Maxwell.Middleware.Logger, [wrong_option: :haah]
+      end
+      raise "ok"
+    end
+  end
+
+  test "Complex log_level for Middleware Logger 1" do
+    assert_raise RuntimeError, "ok", fn ->
+      defmodule TAtom3 do
+        use Maxwell.Builder, [:get, :post]
+        middleware Maxwell.Middleware.Logger, log_level: [
+          error: 400..599
+        ]
+      end
+      raise "ok"
+    end
+  end
+
+  test "Complex log_level for Middleware Logger 2" do
+    assert_raise RuntimeError, "ok", fn ->
+      defmodule TAtom4 do
+        use Maxwell.Builder, [:get, :post]
+        middleware Maxwell.Middleware.Logger, log_level: [
+          info: [1..99, 100, 200..299],
+          warn: 300..399,
+          error: :default
+        ]
+      end
+      raise "ok"
+    end
+  end
+
+  test "Complex log_level with wrong status code" do
+    assert_raise ArgumentError, "Logger Middleware: status code only accepts Integer and Range.", fn ->
+      defmodule TStatusCode do
+        use Maxwell.Builder, [:get, :post]
+        middleware Maxwell.Middleware.Logger, log_level: [
+          info: ["100"],
+        ]
+      end
+      raise "ok"
+    end
+  end
+
+  test "Complex log_level with duplicated default level" do
+    assert ExUnit.CaptureLog.capture_log(fn ->
+      defmodule TDefaultLevel1 do
+        use Maxwell.Builder, [:get, :post]
+        middleware Maxwell.Middleware.Logger, log_level: [
+          info: :default,
+          info: :default,
+        ]
+      end
+    end) =~ ~r"\e\[33m\n\d+:\d+:\d+.\d+ \[warn\]  Logger Middleware: default level defined multiple times.\n\e\[0m"
+  end
+
+  test "Complex log_level with conflictive default level" do
+    assert_raise ArgumentError, "Logger Middleware: default level conflict.", fn ->
+      defmodule TDefaultLevel2 do
+        use Maxwell.Builder, [:get, :post]
+        middleware Maxwell.Middleware.Logger, log_level: [
+          info: :default,
+          error: :default,
+        ]
       end
       raise "ok"
     end
